c39f56281a98d7247a4b3531595128d37e1ee401
Jeffrey H <Jeffreyzyh@gmail.com>
2023-05-03 19:43:47 -0400

    Added API skeletons

---
+++ littlelemon/restaurant/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework.serializers import ModelSerializer
+from .models import MenuItem, Booking
+
+class MenuSerializer(ModelSerializer):
+    class Meta:
+        model = MenuItem
+        fields = "__all__"
+
+class BookingSerializer(ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = "__all__"
